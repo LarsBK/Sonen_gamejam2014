b760bf45c023a61417d8d9f0b918d6feb69127b2
Lars Bjørlykke Kristiansen <lars.bjorlykke.kristiansen@gmail.com>
2014-03-15 11:52:02 +0100

    Stronger, faster, better camera and movement

--- Game/Assets/CameraMove.cs
+++ Game/Assets/CameraMove.cs
@@ -5,6 +5,7 @@ public class CameraMove : MonoBehaviour {
 
 	public GameObject follow = null;
 	public float deadZone = 10;
+	public float stopZone = 4;
 	public float moveSpeed = 10;
 
 	private bool moving = true;
@@ -19,9 +20,8 @@ public class CameraMove : MonoBehaviour {
 		if (follow) {
 			Vector2 moveTo = follow.transform.position;
 			Vector2 current = transform.position;
-			print ((moveTo - current).magnitude);
 			float distance = (moveTo - current).magnitude;
-			if (distance <= 1) {
+			if (distance <= stopZone) {
 				moving = false;
 			} else if (moving || distance > deadZone) {
 				moving = true;
--- Game/Assets/PlayerController.cs
+++ Game/Assets/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour {
 
 	public int controllerNum = 1;
 	public float moveForce = 10;
+	public float slowDownForce = 10;
 	public float maxSpeed = 10;
 	public float rotationSpeed = 360;
 
@@ -30,21 +31,32 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate() {
 
-		//Limit speed
-		if (Mathf.Abs (rigidbody2D.velocity.x) >= maxSpeed && ((rigidbody2D.velocity.x * moveVector.x) > 0))
-			moveVector.x = 0;
-		if (Mathf.Abs (rigidbody2D.velocity.y) >= maxSpeed && ((rigidbody2D.velocity.y * moveVector.y) > 0))
-			moveVector.y = 0;
+		Vector2 forceVector = moveVector;
+
+		if (moveVector.magnitude > 0.5) { //speed up
+			//Limit speed
+			if (Mathf.Abs (rigidbody2D.velocity.x) >= maxSpeed && ((rigidbody2D.velocity.x * moveVector.x) > 0))
+				forceVector.x = 0;
+			if (Mathf.Abs (rigidbody2D.velocity.y) >= maxSpeed && ((rigidbody2D.velocity.y * moveVector.y) > 0))
+				forceVector.y = 0;
+			forceVector *= moveForce;
+		} else { //Slow down
+			forceVector = rigidbody2D.velocity * (float) -1.0;
+			forceVector.Normalize();
+			forceVector *= slowDownForce;
+		}
+
+		print (rigidbody2D.velocity);
 
 		//Apply move force
-		rigidbody2D.AddForce (moveVector * (moveForce * Time.fixedDeltaTime));
+		rigidbody2D.AddForce (forceVector * Time.fixedDeltaTime);
 
 		//Rotate accoring to aim
 		if (aimVector.magnitude > 0.5) {
 			aimVector.Normalize ();
 			float aimAngle = (float) (((Mathf.Atan2(aimVector.x, aimVector.y) * 180.0)) / (((double)Mathf.PI)));
 			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, aimAngle), rotationSpeed*Time.fixedDeltaTime);
-		}
+		} 
 
 	}
 }
